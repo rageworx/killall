aae32a7064200ecb310cdda115e28bcb3216707f
Raphael Kim <rageworx@gmail.com>
2022-09-20 11:47:24 +0900

    simple optarg implemented, optarg_long is next.

--- src/main.cpp
+++ src/main.cpp
@@ -1,58 +1,349 @@
+#include <unistd.h>
+// -- windows related --
 #include <windows.h>
 #include <process.h>
 #include <Tlhelp32.h>
 #include <winbase.h>
-
+// ---------------------
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
 #include <string>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-void killProcessByName( const char *filename )
+// some POSIX arguments may ignored in Windows, as we know.
+static int optpar_exact = 0;
+static int optpar_prgroup = 0;
+static int optpar_interactive = 0;
+static int optpar_list = 0;
+static int optpar_ns = 0;
+static int optpar_olderthan = 0;
+static int optpar_quiet = 0;
+static int optpar_ragexp = 0;
+static int optpar_signal = SIGTERM;
+static int optpar_verbose = 0;
+static int optpar_wait = 0;
+static int optpar_youngerthan = 0;
+static int optpar_context = 0;
+static int optpar_ignorecase = 0;
+static int opterr_notsupported = 0;
+static int opterr_notimplemented = 0;
+static string optpar_param_s;
+static vector<string> plist;
+
+// Windows Processing Killing method -
+void killProcessByName( const char* filename, bool enm )
 {
     HANDLE hSnapShot = CreateToolhelp32Snapshot( TH32CS_SNAPALL, 0 );
     PROCESSENTRY32 pEntry;
     pEntry.dwSize = sizeof (pEntry);
     BOOL hRes = Process32First( hSnapShot, &pEntry );
 
-    while (hRes)
+    while ( hRes )
     {
-        if ( strcmp(pEntry.szExeFile, filename) == 0 )
+        bool dorm = false;
+
+        string curprocname = pEntry.szExeFile;
+        string targetname  = filename;
+
+        // let case change if option enabled.
+        if ( optpar_ignorecase > 0 )
+        {
+            transform( curprocname.begin(), curprocname.end(),
+                       curprocname.begin(), ::tolower );
+            transform( targetname.begin(), targetname.end(),
+                       targetname.begin(), ::tolower );
+        }
+
+        if ( enm == true )
+        {
+            if ( curprocname == targetname )
+                dorm = true;
+        }
+        else
+        {
+            if ( curprocname.find( targetname ) != string::npos )
+                dorm = true;
+        }
+
+        if ( dorm == true )
         {
             HANDLE hProcess = OpenProcess( PROCESS_TERMINATE, 0,
-                                           (DWORD)pEntry.th32ProcessID);
-            if (hProcess != NULL)
+                                           (DWORD)pEntry.th32ProcessID );
+            if ( hProcess != NULL )
             {
-                TerminateProcess(hProcess, 9);
-                CloseHandle(hProcess);
+                bool bskip = false;
+
+                if ( ( optpar_interactive > 0 ) && ( optpar_quiet == 0 ) )
+                {
+                    // ask to user.
+                    fprintf( stdout, 
+                             "Process %s may killed, proceed (Y/n)? ",
+                             pEntry.szExeFile );
+                    fflush( stdout );
+                    char tmpbuff[2] = {0};
+                    while (true)
+                    {
+                        fgets( tmpbuff, 2, stdin );
+                        if ( tmpbuff[0] == 'n' )
+                        {
+                            bskip = true;
+                        }
+                        else
+                            break;
+                    }
+                }
+                
+                if ( bskip == false )
+                {
+                    TerminateProcess( hProcess, 9 );
+
+                    if ( ( optpar_verbose > 0 ) && ( optpar_quiet == 0 ) )
+                    {
+                        fprintf( stdout,
+                                 "%s killed.\n",
+                                 pEntry.szExeFile );
+                    }
+                }
+                else
+                if ( optpar_verbose > 0 )
+                {
+                    if ( optpar_quiet == 0 )
+                        fprintf( stdout, 
+                                 "%s not killed.\n", 
+                                 pEntry.szExeFile );
+                }
+
+                CloseHandle( hProcess );
             }
         }
-        hRes = Process32Next(hSnapShot, &pEntry);
+
+        hRes = Process32Next( hSnapShot, &pEntry );
     }
 
     CloseHandle(hSnapShot);
 }
 
+void showSignalNames()
+{
+    fprintf( stdout,
+             "HUP INT QUIT ILL TRAP ABRT IOT BUS FPE KILL USR1 SEGV USR2 PIPE ALRM TERM STKFLT CHLD CONT STOP TSTP TTIN TTOU URG XCPU XFSZ VTALRM PROF WINCH IO PWR SYS UNUSED\n" );
+}
+
+void showVersion()
+{
+    const char aboutIt[] = \
+"Copyright (C) 2022 Raphael Kim (rageworx-at-gmail.com)\n"
+"\n"
+"this killall windows version comes with ABSOLUTELY NO WARRANTY.\n"
+"This is free software, and you are welcome to redistribute it under\n"
+"the terms of the GNU General Public License.\n"
+"For more information about these matters, see the files named COPYING.";
+
+    fprintf( stdout, "%s\n", aboutIt );
+}
+
+void showHelp()
+{
+    const char mankillall[] = \
+"-e, --exact\n"
+"      Require an exact match for very long names.  If a command\n"
+"      name is longer than 15 characters, the full name may be\n"
+"      unavailable (i.e.  it is swapped out).  In this case,\n"
+"      killall will kill everything that matches within the first\n"
+"      15 characters.  With -e, such entries are skipped.\n"
+"      killall prints a message for each skipped entry if -v is\n"
+"      specified in addition to -e.\n"
+"\n"
+"-I, --ignore-case\n"
+"      Do case insensitive process name match.\n"
+"\n"
+"-g, --process-group\n"
+"      Kill the process group to which the process belongs.  The\n"
+"      kill signal is only sent once per group, even if multiple\n"
+"      processes belonging to the same process group were found.\n"
+"\n"
+"-i, --interactive\n"
+"      Interactively ask for confirmation before killing.\n"
+"\n"
+"-l, --list\n"
+"      List all known signal names.\n"
+"\n"
+"-n, --ns\n"
+"      Match against the PID namespace of the given PID. The\n"
+"      default is to match against all namespaces.\n"
+"\n"
+"-o, --older-than\n"
+"      Match only processes that are older (started before) the\n"
+"      time specified.  The time is specified as a float then a\n"
+"      unit.  The units are s,m,h,d,w,M,y for seconds, minutes,\n"
+"      hours, days, weeks, months and years respectively.\n"
+"\n"
+"-q, --quiet\n"
+"      Do not complain if no processes were killed.\n"
+"\n"
+"-r, --regexp\n"
+"      * current version not implemented this. *\n"
+"      Interpret process name pattern as a POSIX extended regular\n"
+"      expression, per regex(3).\n"
+"\n"
+"-s, --signal, -SIGNAL\n"
+"      * it may not availed on Windows. *\n"
+"      Send this signal instead of SIGTERM.\n"
+"\n"
+"-u, --user\n"
+"      Kill only processes the specified user owns.  Command\n"
+"      names are optional.\n"
+"\n"
+"-v, --verbose\n"
+"      Report if the signal was successfully sent.\n"
+"\n"
+"-V, --version\n"
+"      Display version information.\n"
+"\n"
+"-w, --wait\n"
+"      * it may not availed on Windows. *\n"
+"      Wait for all killed processes to die.  killall checks once\n"
+"      per second if any of the killed processes still exist and\n"
+"      only returns if none are left.  Note that killall may wait\n"
+"      forever if the signal was ignored, had no effect, or if\n"
+"      the process stays in zombie state.\n"
+"\n"
+"-y, --younger-than\n"
+"      * this version not availed for this option *\n"
+"      Match only processes that are younger (started after) the\n"
+"      time specified.  The time is specified as a float then a\n"
+"      unit.  The units are s,m,h,d,w,M,y for seconds, minutes,\n"
+"      hours, days, weeks, Months and years respectively.\n"
+"\n"
+"-Z, --context\n"
+"      Specify security context: kill only processes having\n"
+"      security context that match with given extended regular\n"
+"      expression pattern.  Must precede other arguments on the\n"
+"      command line.  Command names are optional.";
+
+    fprintf( stdout, "%s\n", mankillall );
+}
+
 int main( int argc, char** argv )
 {
-    if ( argc > 1 )
+    // get names 
+    for ( int cnt=1; cnt<argc; cnt++ )
     {
-        for( int cnt=1; cnt<argc; cnt++ )
+        if ( strlen( argv[cnt] ) > 0 )
         {
-            string dstnm = argv[cnt];
+            if ( argv[cnt][0] != '-' )
+            {
+                plist.push_back( argv[cnt] );
+            }
+        }
+    }
 
+    // getopt
+    for(;;)
+    {
+        int opt = getopt( argc, argv, ":heIgilnoqrsuvVwyZ" );
+        if ( opt >= 0 )
+        {
+            switch( (char)opt )
+            {
+                default:
+                case 'h':
+                    showHelp();
+                    return 0;
+
+                case 'l':
+                    showSignalNames();
+                    return 0;
+
+                case 'V':
+                    showVersion();
+                    return 0;
+
+                case 'v':
+                    optpar_verbose = 1;
+                    break;
+
+                case 'q':
+                    optpar_quiet = 1;
+                    break;
+
+                case 'i':
+                    optpar_interactive = 1;
+                    break;
+
+                case 'I':
+                    optpar_ignorecase = 1;
+                    break;
+
+                case 'g':
+                case 'u':
+                case 'w':
+                    // unsupported on windows.
+                    opterr_notsupported = 1;
+                    break;
+
+                case 's':
+                case 'r':
+                case 'o':
+                case 'y':
+                case 'Z':
+                    // not implemented options.
+                    opterr_notimplemented = 1;
+                    break;
+            }
+        }
+        else
+            break;
+    }
+
+    if ( plist.size() == 0 )
+    {
+        if ( optpar_quiet == 0 )
+        {
+            if ( opterr_notsupported > 0 )
+            {
+                fprintf( stdout, "%s\n", "not supported feature." );
+            }
+            else
+            if ( opterr_notimplemented > 0 )
+            {
+                fprintf( stdout, "%s\n", "not implemented feature." );
+            }
+        }
+    }
+#ifdef DEBUG    
+    else
+    {
+        for( size_t cnt=0; cnt<plist.size(); cnt++ )
+        {
+            printf( "kill list [%03zu] %s\n", cnt, plist[cnt].c_str() );
+        }
+    }
+#endif /// of DEBUG
+
+    for( size_t cnt=0; cnt<plist.size(); cnt++ )
+    {
+        bool parm = false;
+        string dstnm = plist[cnt];
+
+        if ( optpar_exact > 0 )
+        {
             size_t fpos = dstnm.find_last_of( "." );
 
             if ( fpos == string::npos )
             {
+                // maybe need EXE extention for Windows as default.
                 dstnm += ".exe";
             }
-                              
-            killProcessByName( dstnm.c_str() );
 
+            parm = true;
         }
+
+        killProcessByName( dstnm.c_str() , parm );
     }
 
     return 0;
